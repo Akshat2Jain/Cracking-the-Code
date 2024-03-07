6874f3229bf75049ef88c078b83ac03fabdfdce9
Akshatjain <akshat.2024it1031@kiet.edu>
2024-03-07 19:45:01 +0530

    added buy stock and sell 1

--- Arrays/7_LeetCode_Arrays9.cpp
+++ Arrays/7_LeetCode_Arrays9.cpp
@@ -16,7 +16,23 @@ using namespace std;
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
+int maximizeProfit(vector<int> &vec)
+{
+    int mini = vec[0];
+    int profit = 0;
+    for (int i = 1; i < vec.size(); i++)
+    {
+        int cost = vec[i] - mini;
+        profit = max(profit, cost);
+        mini = min(mini, vec[i]);
+    }
+    return profit;
+}
+
 int main()
 {
-    cout << "Hello World!";
+    vector<int> vec;
+    vec = {7, 1, 5, 3, 6, 4};
+    int ans = maximizeProfit(vec);
+    cout << "The maximum profit earned is:" << ans << endl;
 }
