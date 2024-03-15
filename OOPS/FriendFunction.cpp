408bfa4f968cdd42aa2bb4bcb2e30792a364c80e
Akshatjain <akshat.2024it1031@kiet.edu>
2024-03-15 20:22:49 +0530

    add

--- OOPS/FriendFunction.cpp
+++ OOPS/FriendFunction.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+// A friend class can access private and protected members of other class in which it is declared as friend.
+class A
+{
+    int x;
+
+public:
+    A()
+    {
+        x = 10;
+    }
+    friend class B; // friend class
+};
+
+class B
+{
+public:
+    void display(A &t)
+    {
+        cout << endl
+             << "The value of x=" << t.x;
+    }
+};
+
+int main()
+{
+    A _a;
+    B _b;
+    _b.display(_a);
+    return 0;
+}
